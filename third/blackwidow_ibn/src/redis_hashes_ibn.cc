bd0fb9e0d0cec7c394998981441d3817dd20bf35
luowencai <luowencai@ibingniao.com>
2020-04-24 16:53:07 +0800

    *

--- third/blackwidow_ibn/src/redis_hashes_ibn.cc
+++ third/blackwidow_ibn/src/redis_hashes_ibn.cc
@@ -93,121 +93,6 @@ namespace blackwidow {
     }
 
 
-    // Status RedisHashes::BNHistoryRange1(const Slice &key, const Slice &field, const Slice &history_filed,
-    //                                 int64_t value, int64_t r_val, int32_t *ret) {
-    //   rocksdb::WriteBatch batch;
-    //   ScopeRecordLock l(lock_mgr_, key);
-
-    //   int32_t version = 0;
-    //   uint32_t statistic = 0;
-    //   std::string meta_value;
-    //   Status s = db_->Get(default_read_options_, handles_[0], key, &meta_value);
-    //   if (s.ok()) {
-    //     ParsedHashesMetaValue parsed_hashes_meta_value(&meta_value);
-    //     if (parsed_hashes_meta_value.IsStale()
-    //         || parsed_hashes_meta_value.count() == 0) {
-    //       version = parsed_hashes_meta_value.InitialMetaValue();
-    //       parsed_hashes_meta_value.set_count(2);
-    //       batch.Put(handles_[0], key, meta_value);
-    //       HashesDataKey hashes_data_key(key, version, field);
-    //       HashesDataKey hashes_max_key(key, version, history_filed);
-
-    //       char buf[32];
-    //       Int64ToStr(buf, 32, value);
-    //       batch.Put(handles_[1], hashes_data_key.Encode(), buf);
-    //       batch.Put(handles_[1], hashes_max_key.Encode(), buf);
-    //       *ret = 1;
-    //     } else {
-    //       bool over_range = false;
-    //       bool save_max = false;
-    //       int32_t count = 0;
-    //       std::string data_value;
-    //       version = parsed_hashes_meta_value.version();
-    //       HashesDataKey hashes_max_key(key, version, history_filed);
-    //       s = db_->Get(default_read_options_, handles_[1],
-    //                    hashes_max_key.Encode(), &data_value);
-    //       if(s.ok()){
-    //         int64_t ival = 0;
-    //         if (!StrToInt64(data_value.data(), data_value.size(), &ival)) {
-    //           return Status::Corruption("hash value is not an integer");
-    //         }
-
-    //         if (value > ival) {//更新最大值
-    //           statistic++;
-    //           save_max = true;
-    //           if(value - ival > r_val) { //超出范围
-    //             over_range = true;
-    //           }
-    //         }
-    //       } else if (s.IsNotFound()) {
-    //           over_range = true;
-    //           save_max = true;
-    //           count++;
-    //       } else {
-    //           return s;
-    //       }
-
-    //       bool save_min = false;
-    //       HashesDataKey hashes_data_key(key, version, field);
-    //       s = db_->Get(default_read_options_, handles_[1],
-    //                    hashes_data_key.Encode(), &data_value);
-    //       if(s.ok()){
-    //         int64_t ival = 0;
-    //         if (!StrToInt64(data_value.data(), data_value.size(), &ival)) {
-    //           return Status::Corruption("hash value is not an integer");
-    //         }
-
-    //         if(value < ival){//更新最小值
-    //           statistic++;
-    //           save_min = true;
-    //         }
-    //       } else if (s.IsNotFound()) {
-    //         count++;
-    //         save_min = true;
-            
-    //         if(over_range){// 当前值为超出范围时
-    //           *ret = 1;
-    //         }
-    //       } else {
-    //         return s;
-    //       }
-    //       if(save_min || save_max){
-    //           char buf[32];
-    //           Int64ToStr(buf, 32, value);
-    //           if(save_max) {
-    //             batch.Put(handles_[1], hashes_max_key.Encode(), buf);
-    //           }
-    //           if(save_min) {
-    //             batch.Put(handles_[1], hashes_data_key.Encode(), buf);
-    //           } 
-    //       }
-    //       if(count != 0){
-    //         parsed_hashes_meta_value.ModifyCount(count);
-    //         batch.Put(handles_[0], key, meta_value);
-    //       }
-    //     }
-    //   } else if (s.IsNotFound()) {// 数据的初始化
-    //     char str[4];
-    //     EncodeFixed32(str, 1);
-    //     HashesMetaValue hashes_meta_value(std::string(str, sizeof(int32_t)));
-    //     version = hashes_meta_value.UpdateVersion();
-    //     batch.Put(handles_[0], key, hashes_meta_value.Encode());
-    //     HashesDataKey hashes_data_key(key, version, field);
-    //     HashesDataKey hashes_max_key(key, version, history_filed);
-
-    //     char buf[32];
-    //     Int64ToStr(buf, 32, value);
-    //     batch.Put(handles_[1], hashes_data_key.Encode(), buf);
-    //     batch.Put(handles_[1], hashes_max_key.Encode(), buf);
-    //     *ret = 1;
-    //   } else {
-    //     return s;
-    //   }
-    //   s = db_->Write(default_write_options_, &batch);
-    //   UpdateSpecificKeyStatistics(key.ToString(), statistic);
-    //   return s;
-    // }
-
     Status RedisHashes::BNHistoryRange(const Slice &key, const Slice &field, const Slice &history_filed,
                                     int64_t value, int64_t r_val, int32_t *ret) {
       rocksdb::WriteBatch batch;
@@ -233,54 +118,72 @@ namespace blackwidow {
           batch.Put(handles_[1], hashes_max_key.Encode(), buf);
           *ret = 1;
         } else {
-          version = parsed_hashes_meta_value.version();
+          bool over_range = false;
+          bool save_max = false;
+          int32_t count = 0;
           std::string data_value;
+          version = parsed_hashes_meta_value.version();
           HashesDataKey hashes_max_key(key, version, history_filed);
           s = db_->Get(default_read_options_, handles_[1],
                        hashes_max_key.Encode(), &data_value);
-          char buf[32];
-          Int64ToStr(buf, 32, value);
           if(s.ok()){
-              int64_t ival = 0;
-              if (!StrToInt64(data_value.data(), data_value.size(), &ival)) {
-                return Status::Corruption("hash value is not an integer");
-              }
+            int64_t ival = 0;
+            if (!StrToInt64(data_value.data(), data_value.size(), &ival)) {
+              return Status::Corruption("hash value is not an integer");
+            }
 
+            if (value > ival) {//更新最大值
               statistic++;
-              if (value > ival) {//更新最大值
-                batch.Put(handles_[1], hashes_max_key.Encode(), buf);
+              save_max = true;
+              if(value - ival > r_val) { //超出范围
+                over_range = true;
               }
-              // batch.Put(handles_[1], hashes_max_key.Encode(), buf);
+            }
           } else if (s.IsNotFound()) {
-            parsed_hashes_meta_value.ModifyCount(1);
-            batch.Put(handles_[0], key, meta_value);
-            batch.Put(handles_[1], hashes_max_key.Encode(), buf);
-            
+              over_range = true;
+              save_max = true;
+              count++;
           } else {
-            return s;
+              return s;
           }
 
+          bool save_min = false;
           HashesDataKey hashes_data_key(key, version, field);
           s = db_->Get(default_read_options_, handles_[1],
                        hashes_data_key.Encode(), &data_value);
           if(s.ok()){
-              int64_t ival = 0;
-              if (!StrToInt64(data_value.data(), data_value.size(), &ival)) {
-                return Status::Corruption("hash value is not an integer");
-              }
+            int64_t ival = 0;
+            if (!StrToInt64(data_value.data(), data_value.size(), &ival)) {
+              return Status::Corruption("hash value is not an integer");
+            }
 
+            if(value < ival){//更新最小值
               statistic++;
-              if(value < ival){//更新最小值
-                batch.Put(handles_[1], hashes_data_key.Encode(), buf);
-              }
-              // batch.Put(handles_[1], hashes_data_key.Encode(), buf);
+              save_min = true;
+            }
           } else if (s.IsNotFound()) {
-            parsed_hashes_meta_value.ModifyCount(1);
-            batch.Put(handles_[0], key, meta_value);
-            batch.Put(handles_[1], hashes_data_key.Encode(), buf);
+            count++;
+            save_min = true;
             
+            if(over_range){// 当前值为超出范围时
+              *ret = 1;
+            }
           } else {
             return s;
+          }
+          if(save_min || save_max){
+              char buf[32];
+              Int64ToStr(buf, 32, value);
+              if(save_max) {
+                batch.Put(handles_[1], hashes_max_key.Encode(), buf);
+              }
+              if(save_min) {
+                batch.Put(handles_[1], hashes_data_key.Encode(), buf);
+              } 
+          }
+          if(count != 0){
+            parsed_hashes_meta_value.ModifyCount(count);
+            batch.Put(handles_[0], key, meta_value);
           }
         }
       } else if (s.IsNotFound()) {// 数据的初始化
