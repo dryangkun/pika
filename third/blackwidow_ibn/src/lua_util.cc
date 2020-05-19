ce2bcbbaa8f48b9582999397b063e6bf5fd2c9e2
dryangkun <dryangkun@163.com>
2020-05-19 19:27:07 +0800

    *

--- third/blackwidow_ibn/src/lua_util.cc
+++ third/blackwidow_ibn/src/lua_util.cc
@@ -13,7 +13,7 @@ std::string LuaUtilToString(lua_State* L, int index) {
   return std::string(str, str_len);
 }
 
-LuaUtil::LuaUtilObjStr("_LuaUtilObj_");
+LuaUtil::LuaUtilObjStr = "_LuaUtilObj_";
 
 LuaUtil::LuaUtil() {
 }
